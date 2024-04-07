f636525a02012e714fda2c19e0b81b0ff22f6cc7
deanbred <deanbred@gmail.com>
2024-04-07 01:32:22 -0400

    better

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2b5a086d21b0b7b6"; }
+/******/ 		__webpack_require__.h = function() { return "732a78051d907771"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -41,174 +41,166 @@ export default function Home() {
         <link rel="icon" href="/bg.png" />
       </Head>
 
-      <main
-        id="page-wrap"
-        className="font-ubuntu bg-no-repeat bg-contain bg-black bg-[url('../public/bg.png')]"
-      >
-        <div className="p-1 flex min-h-screen flex-col border-b-2 overflow-hidden">
-          <div className="flex justify-center items-center w-full">
-            <h2 className="text-5xl text-stone-200">MechAnime</h2>
-            {/*               {
-                <Image
-                  src="logo.png"
-                  width={120}
-                  height={120}
-                  alt="Logo"
-                  className="p-2 rounded-3xl"
-                ></Image>
-              } */}
-          </div>
-
-          <div className="p-2 mb-4 mr-auto text-xl space-y-4 flex flex-col items-start">
-            <button
-              className="p-2 bg-[#234bfa] text-stone-200 rounded-xl"
-              onClick={async () =>
-                window.open("https://aerodrome.finance/swap?from=eth&to=")
-              }
-            >
-              Buy Mecha
-            </button>
-
-            <button
-              className="p-2 bg-[#234bfa] text-stone-200 rounded-xl"
-              onClick={async () => window.open("https://dexscreener.com/base/")}
-            >
-              Chart
-            </button>
-
-            <div className="flex flex-col items-center justify-center space-y-4">
-              <SocialIcon
-                url="https://t.me/mech_anime"
-                network="telegram"
-                fgColor="white"
-              />
-              <SocialIcon
-                url="https://twitter.com/mechanime_"
-                network="twitter"
-                fgColor="white"
-              />
+      <div className=" bg-no-repeat xl:bg-contain lg:bg-contain md:bg-contain sm:bg-contain xs:bg-contain bg-contain 2xl:bg-cover bg-[#f1d28e] bg-[url('../public/bg.png')]">
+        <main id="page-wrap" className="font-ubuntu">
+          <div className="p-1 flex min-h-screen flex-col border-b-2 overflow-hidden">
+            <div className="flex justify-center items-center w-full">
+              <h2 className="xl:text-5xl lg:text-4xl md:text-3xl sm:text-3xl xs:text-2xl text-2xl text-stone-200">MechAnime</h2>
             </div>
-          </div>
 
-          <div className="p-2 mt-4 m-auto text-center max-w-lg bg-stone-100 bg-opacity-80 rounded-2xl outline outline-2 outline-[#234bfa]">
-            <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
-              Mechanime pushes the evolution of meme tokens further into art
-              inspired by master game artist Akihiko Yoshida.
-            </p>
-          </div>
+            <div className="p-1 pl-3 mb-4 mr-auto text-xl space-y-4 flex flex-col items-start">
+              <button
+                className="p-1 bg-[#234bfa] text-stone-200 rounded-xl"
+                onClick={async () =>
+                  window.open("https://aerodrome.finance/swap?from=eth&to=")
+                }
+              >
+                Buy Now
+              </button>
 
-          <div
-            className="p-2 mt-4 m-auto text-center max-w-xl hover:outline-white bg-stone-100 bg-opacity-70 md:text-base sm:text-base rounded-2xl outline outline-2 outline-[#234bfa] cursor-pointer"
-            onClick={async () => {
-              try {
-                await navigator.clipboard.writeText(copiedText)
-                setCopiedText("Base CA Copied!")
-              } catch (err) {
-                console.error("Failed to copy text: ", err)
-              }
-            }}
-          >
-            <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
-              {copiedText}
-            </p>
-          </div>
+              <button
+                className="p-1 bg-[#234bfa] text-stone-200 rounded-xl"
+                onClick={async () =>
+                  window.open("https://dexscreener.com/base/")
+                }
+              >
+                Chart
+              </button>
 
-          <div className="flex flex-row justify-center space-x-6 ">
-            <div className="p-2 mt-4 text-center max-w-lg bg-stone-100 bg-opacity-80 md:text-base sm:text-base rounded-2xl outline outline-2 outline-[#234bfa] w-48">
+              <div className="flex flex-col items-center justify-center space-y-4">
+                <SocialIcon
+                  url="https://t.me/mech_anime"
+                  network="telegram"
+                  fgColor="white"
+                />
+                <SocialIcon
+                  url="https://twitter.com/mechanime_"
+                  network="twitter"
+                  fgColor="white"
+                />
+              </div>
+            </div>
+
+            <div className="p-2 mt-4 m-auto text-center max-w-lg bg-stone-100 bg-opacity-80 rounded-2xl outline outline-2 outline-[#234bfa]">
               <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
+                Mechanime pushes the evolution of meme tokens further into art
+                inspired by master game artist Akihiko Yoshida.
+              </p>
+            </div>
+
+            <div
+              className="p-2 mt-4 m-auto text-center max-w-xl hover:outline-white bg-stone-100 bg-opacity-70 md:text-base sm:text-base rounded-2xl outline outline-2 outline-[#234bfa] cursor-pointer"
+              onClick={async () => {
+                try {
+                  await navigator.clipboard.writeText(copiedText)
+                  setCopiedText("Base CA Copied!")
+                } catch (err) {
+                  console.error("Failed to copy text: ", err)
+                }
+              }}
+            >
+              <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
+                {copiedText}
+              </p>
+            </div>
+
+            <div className="flex flex-row justify-center space-x-6 ">
+              <div className="p-2 mt-4 text-center max-w-lg bg-stone-100 bg-opacity-80 md:text-base sm:text-base rounded-2xl outline outline-2 outline-[#234bfa] w-48">
+                <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
+                  <a
+                    className="text-[#234bfa] hover:text-[#ad6544]"
+                    href="https://basescan.org/token/"
+                  >
+                    Renounced Contract
+                  </a>
+                </p>
+                <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
+                  NO TAX
+                </p>
+              </div>
+
+              <div className="p-2 mt-4 text-center max-w-lg bg-stone-100 bg-opacity-80 md:text-base sm:text-base rounded-2xl outline outline-2 outline-[#234bfa] w-48">
+                <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
+                  Supply 42B
+                </p>
+
+                <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
+                  <a
+                    className="text-[#234bfa] hover:text-[#ad6544]"
+                    href="https://basescan.org/address/"
+                  >
+                    Burn
+                  </a>{" "}
+                  6%
+                </p>
+                <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
+                  <a
+                    className="text-[#234bfa] hover:text-[#ad6544]"
+                    href="https://basescan.org/address/"
+                  >
+                    Team
+                  </a>{" "}
+                  3%
+                </p>
+              </div>
+
+              <div className="p-2 mt-4 text-center max-w-lg bg-stone-100 bg-opacity-80 md:text-base sm:text-base rounded-2xl outline outline-2 outline-[#234bfa] w-48">
+                <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
+                  AntiWhale
+                </p>
+                <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
+                  AntiSnipe
+                </p>
+                <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
+                  MaxWallet 3%
+                </p>
+              </div>
+            </div>
+
+            <div className="p-2 mt-4 m-auto text-center max-w-lg bg-stone-100 bg-opacity-80 rounded-2xl outline outline-2 outline-[#234bfa]">
+              <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
+                As chaos led to apocalypse, a few elite families survived in
+                luxury bunkers. Twenty years later, their daughters emerge into
+                a deserted wasteland to fight for last resources on the planet.
+                Follow their story on X.
+              </p>
+            </div>
+
+            <div className="mt-4 p-1 mx-auto">
+              <SlideshowLightbox
+                className="container grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-3 mx-auto"
+                lightboxIdentifier="lightbox1"
+                framework="next"
+                images={images}
+              >
+                {images.map((image) => (
+                  <Image
+                    key={image.src}
+                    src={image.src}
+                    alt={image.alt}
+                    height={500}
+                    width={300}
+                    data-lightboxjs="lightbox1"
+                    quality={80}
+                  />
+                ))}
+              </SlideshowLightbox>
+            </div>
+
+            <div className="m-auto">
+              <p className="lg:text-3xl md:text-2xl sm:text-2xl text-2xl text-stone-100">
                 <a
-                  className="text-[#234bfa] hover:text-[#ad6544]"
-                  href="https://basescan.org/token/"
+                  className="text-stone-100 hover:text-[#234bfa]"
+                  href="https://dexscreener.com/base/"
                 >
-                  Renounced Contract
+                  $MECHANIME
                 </a>
               </p>
-              <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
-                NO TAX
-              </p>
             </div>
-
-            <div className="p-2 mt-4 text-center max-w-lg bg-stone-100 bg-opacity-80 md:text-base sm:text-base rounded-2xl outline outline-2 outline-[#234bfa] w-48">
-              <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
-                Supply 42B
-              </p>
-
-              <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
-                <a
-                  className="text-[#234bfa] hover:text-[#ad6544]"
-                  href="https://basescan.org/address/"
-                >
-                  Burn
-                </a>{" "}
-                6%
-              </p>
-              <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
-                <a
-                  className="text-[#234bfa] hover:text-[#ad6544]"
-                  href="https://basescan.org/address/"
-                >
-                  Team
-                </a>{" "}
-                3%
-              </p>
-            </div>
-
-            <div className="p-2 mt-4 text-center max-w-lg bg-stone-100 bg-opacity-80 md:text-base sm:text-base rounded-2xl outline outline-2 outline-[#234bfa] w-48">
-              <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
-                AntiWhale
-              </p>
-              <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
-                AntiSnipe
-              </p>
-              <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
-                MaxWallet 3%
-              </p>
-            </div>
+            <Ticker />
           </div>
-
-          <div className="p-2 mt-4 m-auto text-center max-w-lg bg-stone-100 bg-opacity-80 rounded-2xl outline outline-2 outline-[#234bfa]">
-            <p className="lg:text-2xl md:text-2xl sm:text-xl text-lg">
-              As chaos led to apocalypse, a few elite families survived in
-              luxury bunkers. Twenty years later, their daughters emerge into a
-              deserted wasteland to fight for last resources on the planet.
-              Follow their story on X.
-            </p>
-          </div>
-
-          <div className="mt-4 p-1 mx-auto">
-            <SlideshowLightbox
-              className="container grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-3 mx-auto"
-              lightboxIdentifier="lightbox1"
-              framework="next"
-              images={images}
-            >
-              {images.map((image) => (
-                <Image
-                  key={image.src}
-                  src={image.src}
-                  alt={image.alt}
-                  height={500}
-                  width={300}
-                  data-lightboxjs="lightbox1"
-                  quality={80}
-                />
-              ))}
-            </SlideshowLightbox>
-          </div>
-
-          <div className="m-auto">
-            <p className="lg:text-3xl md:text-2xl sm:text-2xl text-2xl text-stone-100">
-              <a
-                className="text-stone-100 hover:text-[#234bfa]"
-                href="https://dexscreener.com/base/"
-              >
-                $MechAnime
-              </a>
-            </p>
-          </div>
-          <Ticker />
-        </div>
-      </main>
+        </main>
+      </div>
     </>
   )
 }
